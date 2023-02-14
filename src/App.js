5754d85d4d580719e9f75f699aa67b57fc17d967
tieplk <tieplk@dev.ndex.net>
2023-02-14 15:10:50 +0700

    create pages and add react router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
+import {BrowserRouter as Router , Routes, Route} from 'react-router-dom'
 import './App.css';
+import Home from "./pages/Home";
+import Profile from "./pages/Profile"
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import ForgotPassword from "./pages/ForgotPassword";
+import Offer from "./pages/Offer";
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <h1 className='text-2xl bg-red'>
-          Hello
-      </h1>
-    </div>
+    <>
+    <Router>
+      <Routes>
+          <Route path="/" element={<Home/>}></Route>
+          <Route path="/profile" element={<Profile/>}></Route>
+          <Route path="/sign-in" element={<SignIn/>}></Route>
+          <Route path="/sign-up" element={<SignUp/>}></Route>
+          <Route path="/forgot-password" element={<ForgotPassword/>}></Route>
+          <Route path="/offer" element={<Offer/>}></Route>
+      </Routes>
+    </Router>
+    </>
   );
 }
 
