d7cc3ee61e41794abffa01abd99be8b08f23a7ec
tieplk <tieplk@dev.ndex.net>
2023-02-24 11:59:35 +0700

    Complete the forgot password page functionality

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -28,17 +28,17 @@ export default function Header() {
                 <ul className="flex space-x-10">
                     <li className={` cursor-pointer py-3 text-semibold
                      text-gray-400 border-b-[3px] border-b-transparent 
-                     ${pathMathRoute("/") && "text-black border-b-red-500"}`}
+                     ${pathMathRoute("/") && "text-black !border-b-red-500 "}`}
                      onClick={()=>navigate("/")}
                      >Home</li>
                     <li className={` cursor-pointer py-3 text-semibold
                      text-gray-400 border-b-[3px] border-b-transparent 
-                     ${pathMathRoute("/offer") && "text-black border-b-red-500"}`}
+                     ${pathMathRoute("/offer") && "text-black !border-b-red-500"}`}
                      onClick={()=>navigate("/offer")}
                      >Offer</li>
                     <li className={` cursor-pointer py-3 text-semibold
                      text-gray-400 border-b-[3px] border-b-transparent 
-                     ${pathMathRoute("/sign-in") && "text-black border-b-red-500"}`}
+                     ${pathMathRoute("/sign-in") && "text-black !border-b-red-500"}`}
                      onClick={()=>navigate("/sign-in")}
                      >Sign in</li>
 
--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -1,5 +1,7 @@
+import { getAuth, sendPasswordResetEmail } from 'firebase/auth';
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
+import { toast } from 'react-toastify';
 import OAuth from '../components/OAuth';
 export default function ForgotPassword() {
 
@@ -9,6 +11,18 @@ export default function ForgotPassword() {
 
     setEmail(e.target.value);
   }
+
+  async function onSubmit(e){
+    e.preventDefault();
+    try {
+      const auth = getAuth();
+      await sendPasswordResetEmail(auth,email);
+      toast.success("Email was sent");
+    } catch (error) {
+      toast.error("Could not send reset password");
+    }
+  }
+
   return (
     <section>
       <h1 className="text-3xl text-center mt-6 font-bold">Forgot password</h1>
@@ -20,7 +34,7 @@ export default function ForgotPassword() {
            />
         </div>
         <div className="w-full md:w-[50%] lg:w-[40%] lg:ml-20"> 
-          <form >
+          <form onSubmit={onSubmit}>
             <input 
               type="text" 
               name="" 
